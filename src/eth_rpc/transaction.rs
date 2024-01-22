12506bbc92a82675d1ab0b7c46792ce045995d82
crypdoughdoteth <zkorn1@icloud.com>
2024-01-22 14:20:24 -0500

    "eth_getTransactionReceipt + Test + Type system update for smoother API"

--- src/eth_rpc/transaction.rs
+++ src/eth_rpc/transaction.rs
@@ -1,25 +1,19 @@
+use axum::http::Uri;
 use serde_json::json;
 
 use crate::json_rpc::types::{JsonRpcRequest, JsonRpcResponse};
 
 use super::{
     errors::EthCallError,
-    types::{EthCall, GetTransactionByHash, ResultWrapper, Transaction, ETHEREUM_ENDPOINT},
+    types::{EthCall, RawGetTransactionByHashResponse, ResultWrapper, Receipt, GetTransactionByHash, RawGetTransactionReceiptResponse},
 };
 
-impl GetTransactionByHash {
-    pub fn new(hash: String) -> GetTransactionByHash {
-        Self { data: hash }
-    }
-}
-
 impl EthCall for GetTransactionByHash {
-    type Inner = JsonRpcResponse<ResultWrapper<Transaction>>;
+    type Inner = JsonRpcResponse<ResultWrapper<RawGetTransactionByHashResponse>>;
 
-    async fn call(&self) -> Result<Self::Inner, EthCallError> {
-        let endpoint = ETHEREUM_ENDPOINT.get().unwrap();
+    async fn call(&self, provider: &Uri) -> Result<Self::Inner, EthCallError> {
         let res = reqwest::Client::new()
-            .post(endpoint.url.to_string())
+            .post(provider.to_string())
             .json(&JsonRpcRequest {
                 jsonrpc: "2.0".to_owned(),
                 method: "eth_getTransactionByHash".to_owned(),
@@ -34,11 +28,31 @@ impl EthCall for GetTransactionByHash {
         Ok(res)
     }
 }
+impl EthCall for Receipt {
+    type Inner = JsonRpcResponse<ResultWrapper<RawGetTransactionReceiptResponse>>;
+
+    async fn call(&self, provider: &Uri) -> Result<Self::Inner, EthCallError> {
+        let res = reqwest::Client::new()
+            .post(provider.to_string())
+            .json(&JsonRpcRequest {
+                jsonrpc: "2.0".to_owned(),
+                method: "eth_getTransactionReceipt".to_owned(),
+                params: Some(json!([self.0.data])),
+                id: 1,
+            })
+            .send()
+            .await?
+            .json::<Self::Inner>()
+            .await?;
+
+        Ok(res)
+    }
+}
 
 #[cfg(test)]
 pub mod tests {
 
-    use crate::eth_rpc::types::{Endpoints, GetTransactionByHash, ETHEREUM_ENDPOINT};
+    use crate::eth_rpc::types::{Endpoints, GetTransactionByHash, ETHEREUM_ENDPOINT, Receipt};
 
     #[tokio::test]
     async fn get_tx_by_hash() -> Result<(), Box<dyn std::error::Error>> {
@@ -47,6 +61,15 @@ pub mod tests {
         let provider = ETHEREUM_ENDPOINT.get().unwrap();
         let args = GetTransactionByHash::new(hash.to_owned());
         provider.get_transaction_by_hash(args).await?;
+        Ok(())
+    }
+    #[tokio::test]
+    async fn get_tx_receipt() -> Result<(), Box<dyn std::error::Error>> {
+        Endpoints::init()?;
+        let hash = "0x10d26a9726e85f6bd33b5a1455219d8d56dd53d105e69e1be062119e8c7808a2";
+        let provider = ETHEREUM_ENDPOINT.get().unwrap();
+        let args = Receipt::new(hash.to_owned());
+        provider.get_transaction_receipt(args).await?;
         Ok(())
     }
 }
--- src/eth_rpc/types.rs
+++ src/eth_rpc/types.rs
@@ -25,7 +25,7 @@ pub enum Chains {
 pub trait EthCall {
     type Inner;
 
-    fn call(&self) -> impl Future<Output = Result<Self::Inner, EthCallError>> + Send;
+    fn call(&self, provider: &Uri) -> impl Future<Output = Result<Self::Inner, EthCallError>> + Send;
 }
 
 #[derive(Clone, Debug, Eq, PartialEq)]
@@ -41,6 +41,16 @@ pub struct GetTransactionByHash {
     pub data: String,
 }
 
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Receipt(pub GetTransactionByHash);
+
+impl Receipt {
+    pub fn new(hash: String) -> Self {
+        let hash = GetTransactionByHash::new(hash);
+        Receipt(hash)
+    }
+}
+
 #[derive(Serialize, Deserialize, Debug)]
 pub struct ResultWrapper<T> {
     result: T,
@@ -48,8 +58,27 @@ pub struct ResultWrapper<T> {
 
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
-pub struct Transaction {
-    #[serde(rename = "accessList")]
+pub struct RawGetTransactionReceiptResponse {
+    transaction_hash: String,
+    transaction_index: String,
+    block_hash: String,
+    block_number: String,
+    from: String,
+    to: String,
+    cumulative_gas_used: String,
+    effective_gas_price: String,
+    gas_used: String,
+    contract_address: Option<String>,
+    logs: Vec<String>,
+    logs_bloom: String,
+    #[serde(rename = "type")]
+    tx_type: String,
+    status: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct RawGetTransactionByHashResponse {
     access_list: serde_json::Value,
     block_hash: String,
     block_number: String,
@@ -72,9 +101,27 @@ pub struct Transaction {
     s: String,
 }
 
+#[derive(Debug, Clone, Serialize)]
+pub struct TransactionData {
+    block_number: u64,
+    chain_id: u16, 
+    from: String, 
+    to: String, 
+    value: u64, 
+    // this is important -- for parsing the calldata sent to a smart
+    // contract if we are being paid in tokens 
+    input: String,
+}
+
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Provider {
     pub url: axum::http::Uri,
+}
+
+impl GetTransactionByHash {
+    pub fn new(hash: String) -> GetTransactionByHash {
+        Self { data: hash }
+    }
 }
 
 impl Provider {
@@ -85,8 +132,19 @@ impl Provider {
     pub async fn get_transaction_by_hash(
         &self,
         args: GetTransactionByHash,
-    ) -> Result<Transaction, EthCallError> {
-        let res = args.call().await?.result.result;
+    ) -> Result<RawGetTransactionByHashResponse, EthCallError> {
+        let res = args.call(&self.url).await?.result.result;
         Ok(res)
     }
+
+    pub async fn get_transaction_receipt(
+        &self,
+        args: Receipt,
+    ) -> Result<RawGetTransactionReceiptResponse, EthCallError> {
+        let res = args.call(&self.url).await?.result.result;
+        Ok(res)
+    }
+
 }
+
+
