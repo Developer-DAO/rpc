e2f6c4885512d845a78781890162f60403a1697a
crypdoughdoteth <zkorn1@icloud.com>
2026-02-15 14:55:46 -0500

    Update IaC dependencies and fix breaking changes

--- infra/opentofu/ecs/backend.tf
+++ infra/opentofu/ecs/backend.tf
@@ -6,11 +6,11 @@ terraform {
           encrypt        = true
       }
 
-  required_version = ">= 1.0.0"
+  required_version = ">= 1.5.7"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5"
+      version = "~> 6.32.1"
     }
   }
 }
